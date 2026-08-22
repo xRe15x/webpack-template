26c26e67a8ba4cfffc4d2ad0be79be24002acf07
xRe15x <266321938+xRe15x@users.noreply.github.com>
2026-08-22 23:34:27 +0100

    typescript

---
+++ webpack.common.ts
@@ -0,0 +1,34 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "node:path";
+import { fileURLToPath } from "url";
+import { type Configuration } from "webpack";
+
+import "webpack-dev-server";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const common: Configuration = {
+    entry: "./src/index.ts",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {test: /\.css$/i, use: ["style-loader", "css-loader"]},
+            {test: /\.ts$/, use: "ts-loader", exclude: /node_modules/}
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    }
+}
+
+export default common;
